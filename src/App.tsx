41f32989dde0185032bdeb27ec4bccebf2ef34c4
Hilful <hilful25@gmail.com>
2022-02-23 01:07:55 +0600

    practicing generic type using title on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import Lists from "./components/Lists";
 
+const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
+  return <div></div>;
+};
+
 function App() {
   return (
     <div className="App">
       <Lists></Lists>
+      <Box title="Hello World!!">Working</Box>
     </div>
   );
 }
