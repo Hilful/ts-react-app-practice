3119f0ab383eb2ef9be52ff44c4cd1dfae537bbc
Hilful <hilful25@gmail.com>
2022-02-23 01:59:23 +0600

    Implemented simple typescript based todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,87 @@
-import React from "react";
+import React, { useCallback, useReducer, useRef, useState } from "react";
 import "./App.css";
-import Lists from "./components/Lists";
 
-const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
-  return <div></div>;
-};
+// const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
+//   return <div></div>;
+// };
+
+interface Todo {
+  id: number;
+  text: string;
+}
+// Todo[]
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
 
 function App() {
+  const [myState, setMyState] = useState<Todo>();
+
+  // useEffect(() => {
+
+  // }, [])
+
+  // function reducer(state: Todo[], action: ActionType) {
+  //   switch (action.type) {
+  //     case "ADD":
+  //       return [
+  //         ...state,
+  //         {
+  //           id: state.length,
+  //           text: action.text,
+  //         },
+  //       ];
+  //     case "REMOVE":
+  //       return state.filter(({ id }) => id !== action.id);
+  //   }
+  // }
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }, []);
+
+  // [{}, {}, {}]
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  // {} === {} // false
+  // useCallback
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
+    // useReducer
+
     <div className="App">
-      <Lists></Lists>
-      <Box title="Hello World!!">Working</Box>
+      {/* <Box title="hello"></Box> */}
+      {/* <Lists /> */}
+
+      <input type="text" ref={newTodoRef} />
+      <button onClick={onAddTodo}>Add</button>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
